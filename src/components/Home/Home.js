fa57d53c7596ace6f4bc55915438db9f996b3b8a
Jamey <nakamajamey+git@gmail.com>
2021-08-06 15:15:12 -0700

    Enable randsense

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,17 +5,42 @@ import Name from '../Name/Name';
 import styles from './Home.module.css';
 
 function Home() {
+  const [sentence, setSentence] = React.useState("");
+
+  React.useEffect(() => {
+    console.log(sentence);
+  }, [sentence]);
+
+  const handleClick = async () => {
+    const response = await fetch("/randsense/api/v1/sentences/", {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    });
+    const data = await response.json();
+    setSentence(data.sentence);
+  }
+
+  console.log(process.env.NODE_ENV);
+
   return (
     <div className={`App ${styles.wrapper}`}>
       <ul className={styles.name}>
         <li><Name name="Jamey"/></li>
         <li><Name name="Joseph"/></li>
-        <li><Name name="Nakama"/></li>
+        <li><Name name="Nakama" url="#" onClick={handleClick}/></li>
       </ul>
       <section className={styles.description}>
-        <p>I'm a software engineer by trade.</p>
-        <p>I'm a fine artist by fun.</p>
-        <p>I like dogs and cats and video games and my wife a lot.</p>
+        {sentence ?
+          <p>{sentence}</p>
+          :
+          <>
+            <p>I'm a software engineer by trade.</p>
+            <p>I'm a fine artist by fun.</p>
+            <p>I like dogs and cats and video games and my wife a lot.</p>
+          </>
+        }
       </section>
     </div>
   );
--- src/components/Name/Name.js
+++ src/components/Name/Name.js
@@ -3,12 +3,18 @@ import Proptypes from 'proptypes';
 
 import styles from './Name.module.css';
 
-const Name = ({name}) => (
-  <a className={styles.name} href="https://www.instagram.com/jameyiguess">{name}</a>
+const Name = ({name, url, onClick}) => (
+  <a className={styles.name} href={url} onClick={onClick}>{name}</a>
 );
 
 Name.propTypes = {
-  name: Proptypes.string
+  name: Proptypes.string,
+  onClick: Proptypes.func
 };
+
+Name.defaultProps = {
+  url: "https://www.instagram.com/jameyiguess",
+  onClick: () => {}
+}
 
 export default Name;
